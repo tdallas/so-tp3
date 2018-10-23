a9a5d782f4bb4e537ad276775f7c30fbfbd97a76
santiagoburgos <burgossantiago94@gmail.com>
2018-10-23 15:08:41 -0300

    Update processExec.c

--- Userland/SampleCodeModule/processExec.c
+++ Userland/SampleCodeModule/processExec.c
@@ -9,18 +9,20 @@ void sysSetForeground(int pid);
 
 int execProcess(void *function, int argc, char **argv, char *name, int foreground)
 {
-	int pid = sysExec(function, argc, argv, name);
-	if (foreground == 1)
-	{
-		sysSetForeground(pid);
-	}
+	int pid;
+	pid = (uint64_t)systemCall(13, (uint64_t)function, argc, (uint64_t)argv, (uint64_t)name, foreground);
+	//int pid = sysExec(function, argc, argv, name);
+	//if (foreground == 1)
+	//{
+	//	sysSetForeground(pid);
+	//}
 	return pid;
 }
 
-int sysExec(void *function, int argc, char **argv, char *name)
-{
-  return (uint64_t)systemCall(13, (uint64_t)function, argc, (uint64_t)argv, (uint64_t)name, 0);
-}
+//int sysExec(void *function, int argc, char **argv, char *name)
+//{
+//  return (uint64_t)systemCall(13, (uint64_t)function, argc, (uint64_t)argv, (uint64_t)name, 0);
+//}
 
 void sysSetForeground(int pid)
 {
